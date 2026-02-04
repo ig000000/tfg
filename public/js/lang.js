74d99a33efa61607d1788e4099400d4ba32c2573
ig000000 <igallegos001@ikasle.ehu.eus>
2026-02-04 15:11:51 +0100

    html nuevo

--- public/js/lang.js
+++ public/js/lang.js
@@ -58,21 +58,59 @@ const translations = {
     invalidLesson: "Lección no válida",
     selectTextfirst: "Selecciona texto primero",
     linkLeson: "Escribe el ID de la lección que quieres enlazar:\n\n",
+    linksOfInterest:"Enlaces de interés",
+
+    //Lista de lecciones
+    listLink:"En esta página se clasifican las lecciones en varias temáticas globales listadas en orden alfabético:",
 
     // Contribution page
-    contributeTitle: "🤝 Guía de Contribución",
-    contributeHow: "Cómo contribuir",
-    contribute1: "Los artículos deben estar relacionados con aprendizaje y tecnología.",
-    contribute2: "Indica siempre el idioma correcto (ES, EU, EN).",
-    contribute3: "Incluye título, autor, fecha y contenido claro.",
+    contributeTitle: "Cómo contribuir",
+    contribute1: "Las aportaciones pueden realizarse en cualquiera de estos tres idiomas: castellano, euskera e inglés. Un/a autor/a puede contribuir con una misma portación traducida a más de uno de dichos idiomas.",
+    contribute2: "Las aportaciones sólo contendrán texto e hiperenlaces a otras páginas web de referencia (incluyendo otras lecciones aprendidas ya publicadas en este repositorio) que sea relevante citar en función del contenido de la aportación.",
+    contribute3: "Formato de una aportación:",
+    contribute4: "Nombre y Apellido del autor/a de la aportación (a efectos de reconocimiento y atribución posterior de la aportación, en virtud de la cláusula BY de la licencia Creative Commons)",
+    contribute5: "Desarrollo de la lección aprendida (máximo 400 palabras)",
 
     // License page
-    licenseTitle: "📜 Licencia y Normas de Cita",
-    licenseInfo: "El contenido puede usarse con fines educativos citando autor y fuente.",
-    quoteRules: "Normas de cita",
-    quote1: "Menciona siempre el autor.",
-    quote2: "Incluye fecha de publicación.",
-    quote3: "Referencia a la web original.",
+    licenseTitle: "Licencia y Normas de Cita",
+    licenseTXT:`<li>A menos que se especifique lo contrario, todas las aportaciones en 
+    "Project Knowledge 14" se encuentran bajo licencia
+    <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en">Creative 
+    Commons Reconocimiento-NoComercial-CompartirIgual 3.0</a>
+    , lo cual significa que eres libre de:
+  </li>
+  <ul>
+    <li><b>Compartir:</b> copiar y redistribuir las aportaciones en cualquier medio o 
+      formato</li>
+    <li><b>Adaptar:</b> remezclar, transformar y crear a partir de las aportaciones</li>
+  </ul>
+  <li>bajo las siguientes condiciones:</li>
+  <ul>
+    <li><b>Reconocimiento:</b> debes <b>reconocer adecuadamente la autoría original de la 
+      aportación</b>, que se indica al comienzo de la entrada del blog que la recoge. 
+      Para ello te pedimos seguir las siguientes indicaciones:
+    </li>
+    <ul>
+      <li>Si la utilizas desde un medio online, con un <b>enlace a la aportación 
+        original</b> (es decir, al enlace permanente de la entrada en este blog), 
+        indicando el <b>nombre del autor</b> que figura al comienzo de la entrada 
+        correspondiente.
+      </li>
+      <li>Si la utilizas desde un medio impreso, el texto debe ir acompañado por 
+        una nota que deje claro al lector <b>dónde puede encontrar la aportación 
+        original</b>, indicando el <b>nombre del autor</b> que figura al comienzo de la entrada
+         correspondiete y la dirección del enlace permanente a dicha entrada 
+         (o si no fuera posible, la dirección de inicio de este blog).
+        </li>
+    </ul>
+    <li><b>NoComercial:</b> No puedes utilizar el material de este blog para una 
+      finalidad comercial.
+    </li>
+    <li><b>CompartirIgual:</b> Si remezclas, transformas o creas a partir del material de 
+      este blog, debes difundir tus contribuciones bajo la misma licencia que el 
+      original.
+    </li>
+  </ul>`,
 
     // Lessons index
     lessonsTitle: "📚 Índice de lecciones",
@@ -138,21 +176,60 @@ const translations = {
     invalidLesson: "Invalid lesson",
     selectTextfirst: "Select text first",
     linkLeson: "Enter the ID of the lesson you want to link:\n\n",
+    linksOfInterest:"Links of Interest",
+
+    //Lista de lecciones
+    listLink:"Para traducir",
 
     // Contribution page
-    contributeTitle: "🤝 Contribution Guidelines",
-    contributeHow: "How to contribute",
-    contribute1: "Articles must be related to learning and technology.",
-    contribute2: "Always indicate language (ES, EU, EN).",
-    contribute3: "Include title, author, date and clear content.",
+    contributeTitle: "How to Contribute",
+    contribute1: "Contributions may be made in any of the following three languages: Spanish, Basque, and English. An author may contribute the same submission translated into more than one of these languages.",
+    contribute2: "Contributions shall contain only text and hyperlinks to other reference web pages (including other lessons learned already published in this repository) that are relevant to cite according to the content of the contribution.",
+    contribute3: "Contribution format:",
+    contribute4: "First name and last name of the author of the contribution (for recognition and subsequent attribution purposes, pursuant to the BY clause of the Creative Commons license).",
+    contribute5: "Development of the lesson learned (maximum 400 words)",
+
 
     // License page
-    licenseTitle: "📜 License & Quoting Guidelines",
-    licenseInfo: "Content may be used for educational purposes with proper citation.",
-    quoteRules: "Quoting rules",
-    quote1: "Always mention the author.",
-    quote2: "Include publication date.",
-    quote3: "Reference the original website.",
+    licenseTitle: "License & Quoting Guidelines ",
+    licenseTXT:`<li>Unless otherwise specified, all contributions in 
+    "Project Knowledge 14" are licensed under the
+    <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en">Creative 
+    Commons Attribution-NonCommercial-ShareAlike 3.0</a>
+    license, which means that you are free to:
+  </li>
+  <ul>
+    <li><b>Share:</b> copy and redistribute the contributions in any medium or 
+      format</li>
+    <li><b>Adapt:</b> remix, transform, and build upon the contributions</li>
+  </ul>
+  <li>under the following conditions:</li>
+  <ul>
+    <li><b>Attribution:</b> you must <b>properly acknowledge the original authorship of the 
+      contribution</b>, which is indicated at the beginning of the blog post in which it is published. 
+      To do so, please follow these guidelines:
+    </li>
+    <ul>
+      <li>If you use it in an online medium, include a <b>link to the original 
+        contribution</b> (i.e., the permanent link to the post on this blog), 
+        indicating the <b>author’s name</b> shown at the beginning of the corresponding post.
+      </li>
+      <li>If you use it in a printed medium, the text must be accompanied by 
+        a note clearly stating <b>where the original contribution can be found</b>, 
+        indicating the <b>author’s name</b> shown at the beginning of the corresponding post
+         and the permanent link to that post 
+         (or, if that is not possible, the homepage URL of this blog).
+        </li>
+    </ul>
+    <li><b>NonCommercial:</b> You may not use the material from this blog for 
+      commercial purposes.
+    </li>
+    <li><b>ShareAlike:</b> If you remix, transform, or build upon the material from 
+      this blog, you must distribute your contributions under the same license as the 
+      original.
+    </li>
+  </ul>`,
+
 
     // Lessons index
     lessonsTitle: "📚 Lessons Index",
@@ -218,21 +295,59 @@ const translations = {
     invalidLesson: "Ikasgai baliogabea",
     selectTextfirst: "Hautatu testua lehenik",
     linkLeson: "Sartu estekatu nahi duzun ikasgaiaren IDa:\n\n",
+    linksOfInterest:"Intereseko estekak",
+
+    //Lista de lecciones
+    listLink:"Para traducir",
 
     // Contribution page
-    contributeTitle: "🤝 Ekartze Gidalerroak",
-    contributeHow: "Nola lagundu",
-    contribute1: "Artikuluek ikasketa eta teknologiarekin lotura izan behar dute.",
-    contribute2: "Beti adierazi hizkuntza (ES, EU, EN).",
-    contribute3: "Sartu izenburua, egilea, data eta eduki argia.",
+    contributeTitle: "Nola lagundu",
+    contribute1: "Ekarpenak hiru hizkuntza hauetako edozeinetan egin daitezke: gaztelaniaz, euskaraz eta ingelesez. Egile batek ekarpen bera hizkuntza horietako batean baino gehiagotan itzulita aurkez dezake.",
+    contribute2: "Ekarpenek testua eta beste webgune batzuetarako estekak baino ez dituzte izango (biltegi honetan jada argitaratutako beste ikasitako lezio batzuk barne), ekarpenaren edukiaren arabera aipatzeko egokiak direnak.",
+    contribute3: "Ekarpen baten formatua:",
+    contribute4: "Ekarpenaren egilearen izen-abizenak (aitorpen eta ondorengo egiletza-esleipen helburuetarako, Creative Commons lizentziaren BY klausularen arabera).",
+    contribute5: "Ikasitako lezioaren garapena (gehienez 400 hitz)",
+
 
     // License page
-    licenseTitle: "📜 Lizentzia eta Aipamen Araudia",
-    licenseInfo: "Edukia helburu hezitzaileekin erabil daiteke, behar bezala aipatuz.",
-    quoteRules: "Aipamen arauak",
-    quote1: "Egilea beti aipatu.",
-    quote2: "Argitalpen data adierazi.",
-    quote3: "Jatorrizko webgunea aipatu.",
+    licenseTitle: "Lizentzia eta Aipamen Jarraibideak ",
+    licenseTXT:`<li>Bestelakorik adierazi ezean, 
+    "Project Knowledge 14" webguneko ekarpen guztiak
+    <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.en">Creative 
+    Commons Aitortu-EzKomertziala-PartekatuBerdin 3.0</a>
+    lizentziapean daude, eta horrek esan nahi du honako hauek libreki egin ditzakezula:
+  </li>
+  <ul>
+    <li><b>Partekatu:</b> ekarpenak edozein euskarri edo 
+      formatutan kopiatu eta birbanatu</li>
+    <li><b>Moldatu:</b> ekarpenak nahastu, eraldatu eta horietan oinarrituta lan berriak sortu</li>
+  </ul>
+  <li>ondorengo baldintzen pean:</li>
+  <ul>
+    <li><b>Aitortza:</b> <b>ekarpenaren jatorrizko egiletza behar bezala aitortu behar duzu</b>, 
+      blogeko sarreraren hasieran adierazten den bezala. 
+      Horretarako, jarraitu honako jarraibide hauek:
+    </li>
+    <ul>
+      <li>Lineako euskarri batean erabiltzen baduzu, <b>jatorrizko ekarpenerako 
+        esteka</b> gehitu behar duzu (hau da, blog honetako sarreraren esteka iraunkorra), 
+        dagokion sarreraren hasieran agertzen den <b>egilearen izena</b> adieraziz.
+      </li>
+      <li>Inprimatutako euskarri batean erabiltzen baduzu, testuak 
+        ohar bat izan behar du irakurleari argi adierazteko <b>non aurki daitekeen jatorrizko ekarpena</b>, 
+        dagokion sarreraren hasieran agertzen den <b>egilearen izena</b> eta sarrera horren 
+        esteka iraunkorraren helbidea adieraziz 
+        (edo, hori posible ez bada, blog honen hasierako helbidea).
+        </li>
+    </ul>
+    <li><b>EzKomertziala:</b> Ezin duzu blog honetako materiala 
+      helburu komertzialekin erabili.
+    </li>
+    <li><b>PartekatuBerdin:</b> Blog honetako materiala nahastu, eraldatu edo horretan 
+      oinarritutako lanak sortzen badituzu, zure ekarpenak jatorrizkoaren lizentzia 
+      beraren pean zabaldu behar dituzu.
+    </li>
+  </ul>`,
 
     // Lessons index
     lessonsTitle: "📚 Ikasgaien Indexa",
